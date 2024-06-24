fcc3a5e8adedce8e4fe31badb1f4b6dec311dd7f
Marat Fatykhov <marat.fa87@gmail.com>
2024-06-24 14:21:56 +0500

    test

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"github.com/ajstarks/svgo"
+	"os"
+)
 
 func main() {
-	fmt.Println("svgo")
+	width := 500
+	height := 500
+	canvas := svg.New(os.Stdout)
+	canvas.Start(width, height)
+
+	lg := []svg.Offcolor{
+		{10, "black", 1},
+		{20, "gray", 1},
+		{100, "lightgray", 1},
+	}
+	canvas.Def()
+	canvas.LinearGradient("lg", 0, 100, 0, 0, lg)
+	canvas.DefEnd()
+
+	x := []int{50, 100, 150, 250, 350}
+	y := []int{50, 100, 50, 65, 40}
+
+	canvas.Polyline(x, y,
+		`fill="url(#lg)"`,
+		`stroke-linejoin="round"`,
+		`stroke-linecap="round"`,
+		`stroke-width="2"`,
+		`stroke="#28AF47"`,
+	)
+
+	canvas.End()
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module svgo_test
 
 go 1.21.0
+
+require github.com/ajstarks/svgo v0.0.0-20211024235047-1546f124cd8b // indirect
